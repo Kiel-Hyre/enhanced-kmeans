ed8129cfc58e0642713ac437b24e2932036625fc
vince-enstack <vince.isip-ext@enstack.com>
2022-05-23 00:30:06 +0800

    url

--- apps/urls.py
+++ apps/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/kmeans/', views.KmeansAPIView.as_view(), name="kmeans"),
+    path('kmeans/', views.KmeansAPIView.as_view(), name="kmeans"),
 ]
