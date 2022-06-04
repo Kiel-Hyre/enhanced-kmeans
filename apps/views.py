b9d33906867305929954e98cb3c3c6317f2946fb
vince-enstack <vince.isip-ext@enstack.com>
2022-06-04 13:09:52 +0800

    sep

--- apps/views.py
+++ apps/views.py
@@ -22,30 +22,16 @@ class KmeansAPIView(APIView):
         n = serializers.FloatField(required=False)
         k = serializers.FloatField(required=False)
 
-    class OutputSerializer(serializers.Serializer):
-        silhouette_score = serializers.FloatField(read_only=True)
-        davies_bouldin_score = serializers.FloatField(read_only=True)
-        calinski_harabasz_score =serializers.FloatField(read_only=True)
-
     def post(self, request, *args, **kwargs):
 
         serializer = self.InputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        path = os.path.abspath(os.getcwd())
-
-        orig = pd.read_csv(serializer.validated_data.get("file", path + "/apps" + "/covid-ph.csv"))
-        data = orig.copy()
-
         try:
-            # minimal cleaning
-            data = data.drop(['REGION', 'PROVINCE', 'CITY'], axis=1)
-        except:
-            None
-
-        return Response(run(
-            data=data,
-            orig=orig,
-            n=serializer.validated_data.get("n", 0.99),
-            k=serializer.validated_data.get("k", 6),
-        ))
+            return Response(run(
+                file=serializer.validated_data.get("file", None),
+                n=serializer.validated_data.get("n", 0.99),
+                k=serializer.validated_data.get("k", 6),
+            ))
+        except Exception as e:
+            return Response({"error": str(e)})
---
+++ run_main.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+
+webbrowser.get().open_new("https://covid19-ekfirefly.herokuapp.com/")
